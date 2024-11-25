047e050fac0274757570c449d0dd46dab60144ec
Tofunmi <109533720+Tofunmi2003@users.noreply.github.com>
2024-11-24 23:02:58 -1200

    updates

--- truffle-config.js
+++ truffle-config.js
@@ -1,30 +1,21 @@
 
 require('dotenv').config();
+
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-require('dotenv').config();
 
-
-
-module.exports = {
-  networks: {
-    sepolia: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://sepolia.infura.io/v3/${INFURA_ID}`),
-      network_id: 11155111,
-      gas: 5500000,
-      gasPrice: 20000000000,
-      confirmations: 2,
-      timeoutBlocks: 200,
-      skipDryRun: true
-      
-    }
+module.networks = {
+  sepolia: {
+    provider: () => new HDWalletProvider(process.env.Mnemonic, `https://eth-sepolia.g.alchemy.com/v2/{process.env.1b9SLyHWL6CZAEN8VykEL4BPah07frT9}`),
+    network_id: 11155111,
+    gas: 5500000,
+    gasPrice: 20000000000,
+    confirmations: 2,
+    timeoutBlocks: 200,
+    skipDryRun: true
   }
-}
-
-module.exports = {
-  // ... other Truffle configuration options
-  compilers: {
-    solc: {
-      version: "0.8.17" // Replace with the desired version
-    }
+};
+compilers = {
+  solc: {
+    version: "0.8.17" // Replace with the desired version
   }
 };
